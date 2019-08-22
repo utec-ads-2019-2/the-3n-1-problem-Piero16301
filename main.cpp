71c5bde72b1172e673a2a9ca6c300fbe2beaf1d2
Piero Angelo <piero.morales@utec.edu.pe>
2019-08-21 20:00:27 -0500

    Fisrt version

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int Lluvia(int a){
+    while (a!=1){
+        if(a%2==0){
+            a /= 2;
+            cout << a << " ";
+        }
+        else{
+            a = (a*3) + 1;
+            cout << a << " ";
+        }
+    }
+    return 1;
+}
+
+int main(){
+    int numero;
+    cin >> numero;
+    cout << Lluvia(numero) << "\n";
     return 0;
 }
